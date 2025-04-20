4156d1e251e7a21933bcb7e7206f801cea94e1ed
Jessica Herrera <herrera.jessica@gmail.com>
2025-04-20 16:00:19 -0400

    Organiza archivos del frontend en su propia rama

---
+++ counter-v3.js
@@ -0,0 +1,22 @@
+console.log("counter-v2.js cargado correctamente");
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetch('https://mb77h5uai8.execute-api.us-east-1.amazonaws.com/getVisitorCount')
+    .then(response => response.json())
+    .then(data => {
+      console.log("Respuesta completa de la API:", data);
+
+      const count = data.count; // aquí el cambio real
+      console.log("Valor de visitas:", count);
+
+      const visitorCountElement = document.getElementById('visitorCount');
+      visitorCountElement.textContent = count ?? "N/A";
+      visitorCountElement.style.color = "#ff0000";
+    })
+    .catch(error => {
+      console.error('Error al obtener el contador de visitas:', error);
+      const visitorCountElement = document.getElementById('visitorCount');
+      visitorCountElement.textContent = "Error";
+      visitorCountElement.style.color = "#ff0000";
+    });
+});
