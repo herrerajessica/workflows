8ea7b5c3ebb9b4cd04157b5a442dfe1bb376d26f
Jessica Herrera <herrera.jessica@gmail.com>
2025-04-21 01:25:41 -0400

    Corrige workflow, actualiza test y elimina README

--- backend/tests/test_lambda_function.py
+++ backend/tests/test_lambda_function.py
@@ -4,10 +4,11 @@ import pytest
 from moto import mock_dynamodb
 
 from backend import lambda_function
+
 @pytest.fixture
 def setup_dynamodb():
     with mock_dynamodb():
-        # Crear recursoss simulado de DynamoDB
+        # Crear recurso simulado de DynamoDB
         dynamodb = boto3.resource("dynamodb", region_name="us-east-1")
         # Crear tabla que tu función Lambda necesita
         table = dynamodb.create_table(
@@ -17,7 +18,7 @@ def setup_dynamodb():
             BillingMode="PAY_PER_REQUEST"
         )
         table.wait_until_exists()
-        yield  # Se ejecuta la pruebas después de esta línea
+        yield  # Se ejecuta la prueba después de esta línea
 
 def test_lambda_handler(setup_dynamodb):
     event = {
